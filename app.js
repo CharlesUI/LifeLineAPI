64c05a0c6edaa6277448ff014aa49b82666c7d07
Osourc <114846045+Osourc@users.noreply.github.com>
2024-07-04 22:53:12 +0800

    changes

--- app.js
+++ app.js
@@ -22,11 +22,21 @@ const connectDb = require('./db/connect')
 
 //middlewares
 app.use(express.json())
-app.use(cors({
+const corsOptions = {
     origin: ["https://tomei-customer.vercel.app"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true
-}))
+};
+
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://tomei-customer.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 //routes
 app.use('/api/v1/admin', adminRouter)
